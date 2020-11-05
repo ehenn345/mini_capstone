c9091865f4a2e14500eb9367d329c4b356f32aa8
Erin Weston <westonerin1@gmail.com>
2020-11-04 20:21:58 -0500

    adds first route

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -1,2 +1,4 @@
 class Api::ProductsController < ApplicationController
+  def all_products
+    return Product.all
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,6 @@ Rails.application.routes.draw do
   #   get "/photos" => "photos#index"
   # end
   namespace :api do 
-    get "/product_path"
+    get "/product_path" => "products#all_products"
+  end
 end
