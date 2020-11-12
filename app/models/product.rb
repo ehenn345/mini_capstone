5f76468c87b2dd860a75f96d2dd04e0c19b07f02
Erin Weston <westonerin1@gmail.com>
2020-11-11 20:19:29 -0500

    adds total and is_discounted method models

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,15 @@
 class Product < ApplicationRecord
   def tax
-   p "i am a tax"
-   price * 1.09
+    price * 0.09
   end
+
+  def is_discounted?
+    price < 10 #will return true or false
+  end
+
+  def total
+    price + tax
+    #price + (price * 0.09)
+  end
+
 end
